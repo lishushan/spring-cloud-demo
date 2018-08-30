4783067b52e381cc311d2172138b011cdb8b237c
storm <648417473@qq.com>
2018-08-30 21:10:31 +0800

    create spring cloud helloworld

---
+++ spring-cloud-service-helloworld/src/main/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting [spring-cloud-service-helloworld]... "
+echo "********************************************************"
+java -jar /usr/local/spring-cloud-demo/spring-cloud-service-helloworld/@project.build.finalName@.jar
