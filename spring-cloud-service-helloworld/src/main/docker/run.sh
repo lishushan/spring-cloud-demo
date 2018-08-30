b96dc6ebe08771ca1fc7b3da477ca9d04089efcc
storm <648417473@qq.com>
2018-08-30 23:18:28 +0800

    change some configurations

--- spring-cloud-service-helloworld/src/main/docker/run.sh
+++ spring-cloud-service-helloworld/src/main/docker/run.sh
@@ -2,4 +2,4 @@
 echo "********************************************************"
 echo "Starting [spring-cloud-service-helloworld]... "
 echo "********************************************************"
-java -jar /usr/local/spring-cloud-demo/spring-cloud-service-helloworld/@project.build.finalName@.jar
+java -jar /usr/local/spring-cloud-service-helloworld/@project.build.finalName@.jar
