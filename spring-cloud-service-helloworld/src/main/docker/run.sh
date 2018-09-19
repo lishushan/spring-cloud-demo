d552c0b52f6ba3fb00202957a9cb4db982414cc0
storm <648417473@qq.com>
2018-09-19 21:40:29 +0800

    delete all

--- spring-cloud-service-helloworld/src/main/docker/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "********************************************************"
-echo "Starting [spring-cloud-service-helloworld]... "
-echo "********************************************************"
-java -jar /usr/local/spring-cloud-service-helloworld/@project.build.finalName@.jar
