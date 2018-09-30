5d87fe54beece1bbbd58de6c741405de40495f60
storm <648417473@qq.com>
2018-09-30 17:07:57 +0800

    change spring-cloud-service-helloworld

--- spring-cloud-service-helloworld/src/main/docker/run.sh
+++ spring-cloud-service-helloworld/src/main/docker/run.sh
@@ -2,4 +2,4 @@
 echo "********************************************************"
 echo "Starting [spring-cloud-service-helloworld]... "
 echo "********************************************************"
-java -jar /mycloud/spring-cloud-service-helloworld/@project.build.finalName@.jar
+java -jar -Xms128m -Xmx512m /mycloud/spring-cloud-service-helloworld/@project.build.finalName@.jar --spring.profiles.active=prod
