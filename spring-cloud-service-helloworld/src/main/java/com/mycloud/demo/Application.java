df4d2fb4c85c8817c0f3c86327a4c4137d1cf35d
storm <648417473@qq.com>
2018-10-10 08:28:07 +0800

    fix it

--- spring-cloud-service-helloworld/src/main/java/com/mycloud/demo/Application.java
+++ spring-cloud-service-helloworld/src/main/java/com/mycloud/demo/Application.java
@@ -1,6 +1,9 @@
 package com.mycloud.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,13 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class Application {
+public class Application implements CommandLineRunner {
+
+    private Logger logger = LoggerFactory.getLogger(Application.class);
 
     @Value("${app.property.env}")
     private String env;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info(String.format("--------------Environment: %s---------------", env));
     }
 
     @GetMapping(value = "/hello")
